b4790451a11833b67251f4165295ae9be9f3686a
Chioma Onyeguke <chiomaonyeguke1@gmail.com>
2021-01-10 18:37:35 +0000

    created table of contents in generateMarkdown.js file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,35 @@
 const fs = require('fs');
 
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answer) {
+  return `
+  # ${answer.title}
+
+  # Table of Contents
+    - [description](#description)
+    - [url](#url)
+    - [purpose](#purpose)
+    - [contributions](#contributions)
+    - [license](#license)
+    - [github](#github)
+    - [email](#email)
+    ## Description of Project
+    ${answer.description}
+    ## Live URL
+    Visit the live application:
+    ${answer.url}
+    ## Purpose of the Project:
+    ${answer.purpose}
+    ## Contributers:
+    ${answer.contributions}
+    ## License:
+    ${answer.license}
+    ## Github Profile:
+    To see the operations behind this project, vist my GitHub profile:
+    ${answer.github}
+    ## Email Address:
+    For any questions about this project, email me at:
+    ${answer.email}
 
 `;
 }
